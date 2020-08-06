8d167a6dc5e2b03a2e15224b890cf7f70c4e4aa7
Nishil Mehta <60040286+nish-03@users.noreply.github.com>
2020-08-07 01:56:55 +0530

    Update and rename NN.py to Scratch-NN.py

--- Neural Network (Scratch)/Scratch-NN.py
+++ Neural Network (Scratch)/Scratch-NN.py
@@ -3,7 +3,7 @@
 """
 Created on Tue Oct 22 23:07:01 2019
 
-@author: nish03,sharang,kartik
+@author: nish03, Sharang Iyer, Kartik Bhide
 """
 
 from mpl_toolkits.mplot3d import Axes3D
@@ -12,29 +12,29 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 final=[]
-df=pd.read_csv('data.csv')
-X1=df[['d','a']]
-X=(X1/np.max(X1))
+df=pd.read_csv('data.csv')      #replace with your file name
+X1=df[['d','a']]                #column names
+X=(X1/np.max(X1))               
 y1=np.array((df['e']),dtype=float)[:, np.newaxis]
 y = y1/(np.max(y1))
-loop=np.arange(0.01,0.11,0.01)
-weights=np.arange(1,11,1)
 pd.set_option('display.max_rows',None)
 pd.set_option('display.max_columns',None)
 pd.set_option('display.width',None)
 np.set_printoptions(threshold=np.inf)
+
 class Neural_Network(object):
     def __init__(self):
-        self.inputSize = 2
+        self.inputSize = 2              
         self.outputSize = 1
-        self.hiddenSize = 8
-        self.hiddenSize2= 8
-        self.hiddenSize3=8
-        self.W1 = np.random.randn(self.inputSize, self.hiddenSize)
+        self.hiddenSize = 8             #neurons in hidden layer 1
+        self.hiddenSize2= 8             #neurons in hidden layer 2
+        self.hiddenSize3=8              #neurons in hidden layer 3      (Additional layers can be added with appropriate changes in the code)
+        self.W1 = np.random.randn(self.inputSize, self.hiddenSize)          #weights
         self.W2 = np.random.randn(self.hiddenSize, self.hiddenSize2)
         self.W3 = np.random.randn(self.hiddenSize2,self.hiddenSize3)
         self.W4 = np.random.randn(self.hiddenSize3,self.outputSize)
-    def forward(self, X):
+        
+    def forward(self, X):                           #forward propagation
         self.z = np.dot(X, self.W1)
         self.z2 = self.sigmoid(self.z) 
         self.z3 = np.dot(self.z2, self.W2) 
@@ -45,13 +45,13 @@ class Neural_Network(object):
         o = self.sigmoid(self.z7)
         return o
 
-    def sigmoid(self, s):
+    def sigmoid(self, s):                   #activation function (can be changed)
         return 1/(1+np.exp(-s))
 
-    def sigmoidPrime(self, s):
+    def sigmoidPrime(self, s):              #derivative of activation
         return s * (1 - s)
 
-    def backward(self, X_train, y_train, o):
+    def backward(self, X_train, y_train, o):        #backward propagation
         self.o_error = y_train - o
         self.o_delta = self.o_error*self.sigmoidPrime(o) 
 
@@ -68,39 +68,25 @@ class Neural_Network(object):
         self.W2 += self.z2.T.dot(self.z4_delta)*0.0005 
         self.W3 += self.z4.T.dot(self.z6_delta)*0.0005
         self.W4 += self.z6.T.dot(self.o_delta)*0.0005
-    def train(self, X_train, y_train):
+        
+    def train(self, X_train, y_train):          #training
         o = self.forward(X_train)
         self.backward(X_train, y_train, o)
     
-    def predict(self):
+    def predict(self):                  #prediction
         print( "Predicted data based on trained weights: ")
         print( "Input (scaled): \n" + str(X_test))
         print( "Output: \n" + str(self.forward(X_test)))
 
 
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.9, random_state=98)
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.9, random_state=98)          #splitting
 error=[]
 NN = Neural_Network()
-for i in range(10000):
-      #  print ("# " + str(i))
-      #  if float(np.mean(((np.abs(y - NN.forward(X)))/y)*100))>10:
-      #        continue         # trains the NN 1,000 times        
-      
-#      print("Input (scaled): \n" + str(X_test))
-      
-#      print("Actual Output: \n" + str(y_test))
-      
-#      print("Predicted Output: \n" + str(NN.forward(X_test)))
+for i in range(10000):          #loop according to requirement
       err=np.mean(((np.abs(y_test - NN.forward(X_test)))/y_test)*100)
       print("% Loss:" + str(np.mean(((np.abs(y_test - NN.forward(X_test)))/y_test)*100))+"\n")
-
-  #  print ("Loss: \n" + str(np.mean(np.square(y_test - NN.forward(X_test)))))
-#          rms = sqrt(mean_squared_error(NN.forward(X_test),y_test))    
- 
       error.append(err)
-    
-  #  print ("\n")
       NN.train(X_train, y_train)
 a=np.min(error)
 print(np.min(error))
